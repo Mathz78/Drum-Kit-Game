99161a452bbb9b04fe4951a018ced71c459e7833
Matheus <matheusrocha7802@gmail.com>
2020-08-02 18:01:20 -0300

    Add listeners to buttons

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+var numberOfButtons = document.querySelectorAll(".drum").length;
+
+for (let i = 0; i < numberOfButtons; i++) {
+    document.querySelectorAll("button")[i].addEventListener("click", function() {
+        alert("I got clicked.");
+    });
+}
