3e445116f9ec64fbb9dc4272a2ca239797549825
Matheus <matheusrocha7802@gmail.com>
2020-08-06 18:43:32 -0300

    Add background images on buttons

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,6 @@ var numberOfButtons = document.querySelectorAll(".drum").length;
 
 for (let i = 0; i < numberOfButtons; i++) {
     document.querySelectorAll("button")[i].addEventListener("click", function() {
-        alert("I got clicked.");
+        console.log("button clicked!!");
     });
 }
