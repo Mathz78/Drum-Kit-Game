e4a86efed7c8c3730e34f8b9505626fc73d3c8d3
Matheus <matheusrocha7802@gmail.com>
2020-08-06 20:21:17 -0300

    Change the color of the text to whiite whenever the button is clicked

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,16 @@
 var numberOfButtons = document.querySelectorAll(".drum").length;
 
 for (let i = 0; i < numberOfButtons; i++) {
-    document.querySelectorAll("button")[i].addEventListener("click", function() {
-        console.log("button clicked!!");
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+        // I can create a variable to refer the button and change the color using the variable
+        // or i can just use "this"
+        
+        // Variable receive the text of the button that have been clicked
+        // var text = this;
+        
+        // Change the color to white;
+        // text.style.color = 'darkcyan';
+        this.style.color = "white";
+
     });
 }
